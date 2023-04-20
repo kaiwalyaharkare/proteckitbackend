349a62b936a889afe5921ec2f22ef551d4d1b939
kai <kai@kais-MacBook-Air.local>
2023-04-19 21:54:58 -0500

    procfile update

--- backend/settings.py
+++ backend/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL ='static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-django_heroku.setttings(locals())
+django_heroku.settings(locals())
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
